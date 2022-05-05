e5b508d6427a060d431285f90593599492eac065
Luke Hands <91289931+HANZY98@users.noreply.github.com>
2022-05-05 15:37:12 +0100

    Creating base function for the game

---
+++ TicTacToe.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+char gameBoard[3][3];
+const char THEPLAYER = 'X'
+const char THECOMPUTER = 'O'
+
+void boardReset();
+void boardPrint();
+int spaceCheck();
+void movePlayer();
+void moveComputer();
+char winnerCheck(); 
+void winnerPrint(char);
+
+int main(){
+
+
+  return 0;
+}
+
+void boardReset(){
+
+}
+
+void boardPrint(){
+
+}
+
+int spaceCheck(){
+
+}
+
+void movePlayer(){
+
+}
+
+void moveComputer(){
+
+}
+
+char winnerCheck(){
+
+}
+
+void winnerPrint(char myWinner){
+
+}
