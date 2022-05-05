5ee26e9bd8e721323b99b19a353bd1526c7616f9
Luke Hands <91289931+HANZY98@users.noreply.github.com>
2022-05-05 16:49:47 +0100

    More functions have been added, specifically, that allow the board to be reset, the board to print and show up within the terminal and to allow the player to choose a move

--- TicTacToe.c
+++ TicTacToe.c
@@ -3,48 +3,94 @@
 #include <ctype.h>
 #include <time.h>
 
-char gameBoard[3][3];
-const char THEPLAYER = 'X'
-const char THECOMPUTER = 'O'
+// 8 minutes in.
 
-void boardReset();
+char gameBoard[3][3];
+const char THEPLAYER = 'X' const char THECOMPUTER = 'O'
+
+    void
+    boardReset();
 void boardPrint();
 int spaceCheck();
 void movePlayer();
 void moveComputer();
-char winnerCheck(); 
+char winnerCheck();
 void winnerPrint(char);
 
-int main(){
+int main()
+{
 
+  char myWinner = ' ';
+
+  boardReset();
+
+  while (myWinner == ' ' && spaceCheck() != 0)
+  {
+    boardPrint();
+
+    movePlayer();
+  }
 
   return 0;
 }
 
-void boardReset(){
+void boardReset()
+{
 
+  for (int x = 0; x < 3; x++)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      gameBoard[x][i] = ' ';
+    }
+  }
 }
 
-void boardPrint(){
-
+void boardPrint()
+{
+  printf(" %c | %c | %c ", gameBoard[0][0], gameBoard[0][1], gameBoard[0][2]);
+  printf("\n---|---|---\n");
+  printf(" %c | %c | %c ", gameBoard[1][0], gameBoard[1][1], gameBoard[1][2]);
+  printf("\n---|---|---\n");
+  printf(" %c | %c | %c ", gameBoard[2][0], gameBoard[2][1], gameBoard[2][2]);
+  printf("\n")
 }
 
-int spaceCheck(){
+int spaceCheck()
+{
+  int spacesFree = 9;
 
+  for (int x = 0; x < 3; x++)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      if (gameBoard[x][i] != ' ')
+      {
+        spacesFree--;
+      }
+    }
+  }
+  return spacesFree;
 }
 
-void movePlayer(){
+void movePlayer()
+{
+  int i;
+  int j;
 
+  printf("Please enter row (1-3): ");
+  scanf("%d", &i);
+  i--;
 }
 
-void moveComputer(){
-
+void moveComputer()
+{
 }
 
-char winnerCheck(){
-
+char winnerCheck()
+{
 }
 
-void winnerPrint(char myWinner){
-
+void winnerPrint(char myWinner)
+{
 }
